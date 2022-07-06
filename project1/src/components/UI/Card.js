a8840ce55b0b1a4ed00e8341e0fd31cb0853964d
kevit-kreena-mavani <kreena.mavani@kevit.io>
2022-07-06 13:02:19 +0530

    project for user name and age

--- project1/src/components/UI/Card.js
+++ project1/src/components/UI/Card.js
@@ -1,7 +1,6 @@
 import React from "react";
 import styles from "./Card.module.css";
-const Card = (props) => {
-  
+const Card = (props) => {  
   return <div className={`${styles.card} ${props.className}`}>{props.children}</div>;
 };
 export default Card;
